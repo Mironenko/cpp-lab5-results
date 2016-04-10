30db638a1b22813ff89b83364c34f89f60278430
KonstantinPronin <prosayfer@mail.ru>
2016-04-10 09:12:40 +0300

    Update ЛР5.cpp

--- ЛР5.cpp
+++ ЛР5.cpp
@@ -42,49 +42,14 @@ class Directory{
 		}
 		
 		void CreateDirectory(string name){
-			Directory New(name);
-			Directory* dptr = &New;
+			Directory* dptr = new Directory(name);
 			DArr.push_back(dptr);
 		}
 		
 		void CreateFile(string fname){
-			SFile New(fname);
-			SFile* fptr = &New;
+			SFile* fptr = new SFile(fname);
 			FArr.push_back(fptr);
 		}
-
-		
-		void RM(string way){
-			int number = 0;
-	
-			for (int i = 0; i < way.size(); i++){
-				if (way[i] == '/') number++;
-			}
-			
-			if (number){
-			
-				int i = 0;
-				string str;
-				string newway;
-				while (way[i] != '/'){
-					str += way[i];
-					i++;
-				}
-				
-				int Next = FindDirectory(str);
-				
-				for (int j = i + 1; j < way.size(); j++)
-					newway += way[j];
-	
-				DArr[Next]->RM(newway);
-			}
-			
-			if (number == 0) {
-				 FArr.erase(FArr.begin() + FindFile(way));	
-			}
-			
-		}
-		
 			
 		int FindDirectory(string target){
 			int i = 0;
@@ -94,26 +59,67 @@ class Directory{
 				
 		int FindFile(string target){
 			int i = 0;
-			while (FArr[i]->name == target) i++;
+			while (FArr[i]->name != target) ++i;
 			return i; 
 		}
 		
 		void DeleteFile(string fname){
 			int index = FindFile(fname);
-			//delete FArr[FArr.begin() + index];
+			delete FArr[index];
 			FArr.erase(FArr.begin() + index);
 			
 		}
 		
 		void DeleteDirectory(string name){
-			DArr.erase(DArr.begin() + FindDirectory(name));
+			int index = FindDirectory(name);
+			delete DArr[index];
+			DArr.erase(DArr.begin() + index);
 		}
+
+		void RM(string way);
+		Directory* operator/ (string name);
 				
 		~Directory(){
 				
 		}
 };
 
+Directory* Directory::operator/ (string name){
+	return DArr[FindDirectory(name)];
+}
+
+void Directory::RM(string way){
+	
+	int number = 0;
+	for (int i = 0; i < way.size(); i++){
+		if (way[i] == '/') number++;
+	}
+		
+	if (number){
+			
+	int i = 0;
+	string str;
+	string newway;
+	
+	while (way[i] != '/'){
+		str += way[i];
+		i++;
+	}
+				
+	int Next = FindDirectory(str);
+				
+	for (int j = i + 1; j < way.size(); j++)
+		newway += way[j];
+	
+	DArr[Next]->RM(newway);
+	}
+			
+	if (number == 0) {
+		int index = FindFile(way);
+		delete FArr[index];
+		FArr.erase(FArr.begin() + index);	
+	}		
+}
 
 
 int main(){
@@ -121,8 +127,11 @@ int main(){
 	Directory First("First");
 	
 	First.CreateFile("MyFile");
-	//cout << First.FindFile("MyFile") << ' ';
-	//First.RM("MyFile");
-	cout << "OK";
+	First.CreateDirectory("Second");
+	(First/"Second") -> CreateFile("MyFile2");
+	First.RM("Second/MyFile2");
+	First.RM("MyFile");
+
+
 	return 0;
 }
