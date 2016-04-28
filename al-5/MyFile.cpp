c09f926bbcabb700ac02f15c15b238170df461a4
Eugene Mironenko <mironenko@rutoken.ru>
2016-04-28 07:47:32 +0300

    Fixed creating files (commented out directories -- not to be used)

--- al-5/MyFile.cpp
+++ al-5/MyFile.cpp
@@ -4,10 +4,6 @@
 
 MyFile::MyFile(shared_ptr<MyFolder> parent, string name, string data) : FileSystemElement(parent, name), data(data)
 {
-	if (parent != shared_ptr<MyFolder>(nullptr)) {
-		cout << "*this:\t" << typeid(*this).name() << endl;
-		parent->add_child(dynamic_pointer_cast<FileSystemElement>(shared_from_this()));
-	}
 }
 
 
